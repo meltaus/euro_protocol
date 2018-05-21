dd9524f8044d7ba3b88fb9e8384872b45e7d0368
meltaus <meltaus@yandex.ru>
2018-05-21 23:33:30 +0700

    Создание файла и класса с описание работы с БД

--- control/workDB.php
+++ control/workDB.php
@@ -1,12 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: bunta
- * Date: 21.05.18
- * Time: 23:21
- */
+
 
 class workDB
 {
+    private $conn;
 
+    //Конструктор. Подключаемся к базе
+    public function __construct()
+    {
+        require_once '../settings/connectionDB.php';
+        $connectionInfo = array("UID" => $user, "PWD" => $password, "Database"=>$database, "APP"=>$nameApp, "CharacterSet"=>$charset);
+        $this->conn = sqlsrv_connect( $host, $connectionInfo);
+    }
+
+    //Закрываем подключение
+    public function __destruct()
+    {
+        if( $this->conn )
+        {
+            sqlsrv_close($this->conn);
+        }
+    }
 }
