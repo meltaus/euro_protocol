f768c09050b7bde56eef804cdbd7114dfb4c692d
meltaus <meltaus@yandex.ru>
2018-05-24 11:10:11 +0700

    Создание базовой формы для входа в систему

--- index.php
+++ index.php
@@ -1,19 +1,3 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Тест</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-    <link rel="stylesheet" href="../css/bootcomplete.css">
-    <script src="../js/bootstrap.js"></script>
-    <script src="../js/jquery-3.2.1.min.js"></script>
-    <script src="../js/bootstrap-typeahead.min.js"></script>
-    <link href="../css/style.css" media="all" rel="stylesheet" type="text/css"/>
-</head>
-
 <?php
-
+include_once $_SERVER["DOCUMENT_ROOT"]."/pages/login.html";
 ?>
