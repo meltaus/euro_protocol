7bcb6e875d7dfac16fc46f21e3cd755361a56dbf
meltaus <meltaus@yandex.ru>
2018-05-26 20:58:47 +0700

    Опиание общих параметров осмотра

--- pages/add_date_see.php
+++ pages/add_date_see.php
@@ -1,7 +1,17 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: bunta
- * Date: 26.05.18
- * Time: 20:57
- */
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Изменить дату осмотра</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    <link rel="stylesheet" href="../css/bootcomplete.css">
+    <script src="../js/bootstrap.js"></script>
+    <script src="../js/jquery-3.2.1.min.js"></script>
+    <script src="../js/bootstrap-typeahead.min.js"></script>
+    <link href="../css/style.css" media="all" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+</body>
