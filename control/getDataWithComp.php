1cf88e9e854b5f711d683058f2a9deac5fac63ea
meltaus <meltaus@yandex.ru>
2018-05-27 22:45:15 +0700

    Добавление сценариев для автодополнения для формы добавление нового протокола

--- control/getDataWithComp.php
+++ control/getDataWithComp.php
@@ -16,5 +16,60 @@ switch ($_GET['mode']) {
         }
         echo json_encode($result);
         break;
+
+    case 'people': // ФИО всех возможных участников
+        $columnName = array("name");
+        $nameArray = $workDB->selectUniqueDataTable("people", $columnName);
+        $iter = count($nameArray);
+        $result = array();
+        for ($i = 0; $i < $iter; $i++) {
+            array_push($result, $nameArray[$i][0]);
+        }
+        echo json_encode($result);
+        break;
+
+    case 'state_car_number': // Гос. Номера всех возожных участников
+        $columnName = array("state_car_number");
+        $nameArray = $workDB->selectUniqueDataTable("people", $columnName);
+        $iter = count($nameArray);
+        $result = array();
+        for ($i = 0; $i < $iter; $i++) {
+            array_push($result, $nameArray[$i][0]);
+        }
+        echo json_encode($result);
+        break;
+
+    case 'model_auto': // Модели всех возможных марок автомобилей
+        $columnName = array("name");
+        $nameArray = $workDB->selectUniqueDataTable("auto", $columnName);
+        $iter = count($nameArray);
+        $result = array();
+        for ($i = 0; $i < $iter; $i++) {
+            array_push($result, $nameArray[$i][0]);
+        }
+        echo json_encode($result);
+        break;
+
+    case 'mark_auto': // Все возможные марки автомобилей
+        $columnName = array("mark");
+        $nameArray = $workDB->selectUniqueDataTable("auto", $columnName);
+        $iter = count($nameArray);
+        $result = array();
+        for ($i = 0; $i < $iter; $i++) {
+            array_push($result, $nameArray[$i][0]);
+        }
+        echo json_encode($result);
+        break;
+
+    case 'number_polis': // Все возможные полисы
+        $columnName = array("number_polis");
+        $nameArray = $workDB->selectUniqueDataTable("polis", $columnName);
+        $iter = count($nameArray);
+        $result = array();
+        for ($i = 0; $i < $iter; $i++) {
+            array_push($result, $nameArray[$i][0]);
+        }
+        echo json_encode($result);
+        break;
 }
 ?>
