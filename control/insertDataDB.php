103df0a7ee429d73c882e47629e84065b6e0e340
meltaus <meltaus@yandex.ru>
2018-05-27 23:48:52 +0700

    Добавление заготовки для обработки формы с insert_notify.php

--- control/insertDataDB.php
+++ control/insertDataDB.php
@@ -1,7 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: bunta
- * Date: 27.05.18
- * Time: 23:39
- */
+include $_SERVER["DOCUMENT_ROOT"] . "/control/workDB.php";
+$workDB = new workDB();
+
+switch ($_GET['mode']) {
+    case "notify":
+        //Сбор данных с полей и внос в БД в соответствующие таблицы при помщи транзакции
+        echo '<meta http-equiv="refresh" content="0; url=/pages/user_space.php">';
+        break;
+}
+?>
