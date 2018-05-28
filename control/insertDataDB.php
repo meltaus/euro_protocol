311221da8f6fae28a89f4c6b98877290789b2812
meltaus <meltaus@yandex.ru>
2018-05-28 21:12:08 +0700

    Проверка уникальности марки и модели автомобиля. Проверка уникальности человека (должно совпадать ФИО, марка и модель автомобиля, гос. номер

--- control/insertDataDB.php
+++ control/insertDataDB.php
@@ -8,7 +8,7 @@ switch ($_GET['mode']) {
 
         //Данные вставляются в таблицы:
         //protocol                      Если дублируются все данные кроме даты - выдавать предупреждение
-        //peolpe                        Если человек есть и совпадает номер полиса - не создавать новую запись
+        //peolpe                        Если человек есть и id_auto и гос. номер - не создавать новую запись
         //notice_for_inspection
         //statement                     Если отличается ФИО, но аналогичный номер есть - выдать предупреждение
         //auto                          Если есть совпадающие марка и авто - новое поле не добавлять
@@ -16,6 +16,22 @@ switch ($_GET['mode']) {
 
         //Проверка уникальности номера полиса
         $query = "SELECT count(number_polis) FROM polis WHERE number_polis =".$_POST['number_polis'];
+        $count = $workDB->analysisResult($workDB->anyQueryDB($query));
+        if ($count[0][0] == 0) {
+        } else {
+        }
+
+        //Проверка уникальности марки и модели авто
+        $query = "SELECT count(id) FROM auto WHERE mark = '".$_POST['mark_auto']."' && `name` = '".$_POST['model_auto']."' ";
+        $count = $workDB->analysisResult($workDB->anyQueryDB($query));
+        if ($count[0][0] == 0) {
+        } else {
+        }
+
+        //Проверка уникальности человека
+        $query = "select count(people.name) from people 
+                  inner join auto on people.id_auto = auto.id where auto.mark = '".$_POST['mark_auto']."' && auto.name = 
+                  '".$_POST['name_auto']."' && people.state_car_number = '".$_POST['state_car_number']."'";
         $count = $workDB->analysisResult($workDB->anyQueryDB($query));
         if ($count[0][0] == 0) {
         } else {
