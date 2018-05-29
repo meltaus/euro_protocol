502a4595baa5fe1466742b66e180b10e50c2e09c
Artem Gorbachev <meltaus@yandex.ru>
2018-05-29 19:04:45 +0700

    Определение необходиных переменных

--- control/insertAllData.php
+++ control/insertAllData.php
@@ -6,6 +6,28 @@ class insertAllData
     private $workDB;
 
     //Переменные, содержащие в себе значения переданных полей
+    private $number_polis_culprit;
+    private $number_polis_member;
+    private $id_statement;
+    private $proxy;
+    private $time_auto_emer;
+    private $time_register;
+    private $FIO_culprit;
+    private $mark_culprit;
+    private $model_culprit;
+    private $state_car_number_culprit;
+    private $FIO_member;
+    private $mark_member;
+    private $model_member;
+    private $state_car_number_member;
+
+    //Переменные присущие только таблице
+    private $id_number_polis;
+    private $id_number_polis_member;
+    private $id_people_culprit;
+    private $id_people_member;
+    private $id_auto_member;
+    private $id_auto_culprit;
 
     //Конструктор
     public function __construct()
@@ -20,9 +42,10 @@ class insertAllData
     }
 
     /**
-     * На вход получает ассоциативный массив, где ключ имя столбца.
+     * Разбор входящих значений
+     * @param $columnValues ассоциативный массив, где ключ имя столбца.
      */
-    public function setData() {
+    public function setData($columnValues) {
 
     }
 
