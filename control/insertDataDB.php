525ee28646c59d91ec8b9c73fa1d1fff1db5b8e1
meltaus <meltaus@yandex.ru>
2018-05-29 22:56:20 +0700

    Вставка данных по протоколу в БД используя класс

--- control/insertDataDB.php
+++ control/insertDataDB.php
@@ -1,6 +1,7 @@
 <?php
-include $_SERVER["DOCUMENT_ROOT"] . "/control/workDB.php";
-$workDB = new workDB();
+//include $_SERVER["DOCUMENT_ROOT"]."/control/workDB.php";
+include $_SERVER["DOCUMENT_ROOT"]."/control/insertAllData.php";
+$insertAllData = new insertAllData();
 
 switch ($_GET['mode']) {
     case "notify":
@@ -14,12 +15,11 @@ switch ($_GET['mode']) {
         //statement
         //auto                          Если есть совпадающие марка и авто - новое поле не добавлять
         //polis                         Всегда уникален
-        //Если count[0][0] == 0 => Сделать вставку в соответствующую таблицу и получить id новой записи
 
         $columnValues = array(
             'number_polis_culprit' => '',
             'number_polis_member' => '',
-            'id_statement' => '',
+            'statement' => '',
             'proxy' => '',
             'time_auto_emer' => '',
             'time_register' => '',
@@ -33,120 +33,15 @@ switch ($_GET['mode']) {
             'state_car_number_member' => '',
         );
 
-        //Проверка уникальности человека
-        $query = "SELECT count(people.name) FROM people 
-                  INNER JOIN auto ON people.id_auto = auto.id WHERE auto.mark = '".$_POST['mark_auto']."' && auto.name = 
-                  '".$_POST['name_auto']."' && people.state_car_number = '".$_POST['state_car_number']."' && 
-                  people.name = '".$_POST['FIO']."'";
-        $count = $workDB->analysisResult($workDB->anyQueryDB($query));
-        if ($count[0][0] == 0) {
-            //Добавить человека и запомнить id добавленного человека
+        $insertAllData->insert($columnValues);
+
+        if($insertAllData->insert()){
+            echo "Все хорошо";
         } else {
-            //Получаем id человека, который совпадает по ФИО, машине и гос.номеру
-            $query = "SELECT people.id FROM people 
-                  INNER JOIN auto ON people.id_auto = auto.id WHERE auto.mark = '".$_POST['mark_auto']."' && auto.name = 
-                  '".$_POST['name_auto']."' && people.state_car_number = '".$_POST['state_car_number']."' && 
-                  people.name = '".$_POST['FIO']."'";
-            $id_people_culprit = $workDB->analysisResult($workDB->anyQueryDB($query));
-        }
-        //Второй участник
-        $query = "SELECT count(people.name) FROM people 
-                  INNER JOIN auto ON people.id_auto = auto.id WHERE auto.mark = '".$_POST['mark_auto_participant']."' && auto.name = 
-                  '".$_POST['name_auto_participant']."' && people.state_car_number = '".$_POST['state_car_number_participant']."' && 
-                  people.name = '".$_POST['FIO_participant']."'";
-        $count = $workDB->analysisResult($workDB->anyQueryDB($query));
-        if ($count[0][0] == 0) {
-            //Добавить человека и запомнить id добавленного человека
-        } else {
-            //Получаем id человека, который совпадает по ФИО, машине и гос.номеру
-            $query = "SELECT people.id FROM people 
-                  INNER JOIN auto ON people.id_auto = auto.id WHERE auto.mark = '".$_POST['mark_auto_participant']."' && auto.name = 
-                  '".$_POST['name_auto_participant']."' && people.state_car_number = '".$_POST['state_car_number_participant']."' && 
-                  people.name = '".$_POST['FIO_participant']."'";
-            $id_people_member = $workDB->analysisResult($workDB->anyQueryDB($query));
+            echo "Все плохо";
         }
 
-        //Проверка уникальности номера полиса
-        $query = "SELECT count(number_polis) FROM polis WHERE number_polis =".$_POST['number_polis'];
-        $count = $workDB->analysisResult($workDB->anyQueryDB($query));
-        if ($count[0][0] == 0) {
-            //добавить полис и запомнить id добавленного полиса
-        } else {
-            $column_name = array("id");
-            $condition = "WHERE number_polis =".$_POST['number_polis'];
-            $id_polis = $workDB->selectDataTableWhere("polis", $column_name, $condition);
-            //Проверяем есть ли существующая комбинация человек - полис
-            if (isset($id_people_culprit)) {
-                $query = "SELECT count(id) FROM protocol WHERE id_number_polis = ".$id_polis[0][0]." && id_people_culprit = ".$id_people_culprit[0][0];
-                $count = $workDB->analysisResult($workDB->anyQueryDB($query));
-                if ($count[0][0] != 0) {
-                    //Есть совпадение по полису и первому участнику
-                }
-
-                $query = "SELECT count(id) FROM protocol WHERE id_number_polis_member = ".$id_polis[0][0]." && id_people_member = ".$id_people_member[0][0];
-                $count = $workDB->analysisResult($workDB->anyQueryDB($query));
-                if ($count[0][0] != 0) {
-                    //Есть совпадение по полису и второму участнику
-                }
-            }
-        }
-
-        //Проверка уникальности марки и модели авто
-        $query = "SELECT count(id) FROM auto WHERE mark = '".$_POST['mark_auto']."' && `name` = '".$_POST['model_auto']."' ";
-        $count = $workDB->analysisResult($workDB->anyQueryDB($query));
-        if ($count[0][0] == 0) {
-        } else {
-            $column_name = array("id");
-            $condition = "WHERE mark = '".$_POST['mark_auto']."' && `name` = '".$_POST['model_auto']."'";
-            $id_auto_people_culprit = $workDB->selectDataTableWhere("auto", $column_name, $condition);
-        }
-        $query = "SELECT count(id) FROM auto WHERE mark = '".$_POST['mark_auto_participant']."' &&
-                  `name` = '".$_POST['model_auto_participant']."' ";
-        $count = $workDB->analysisResult($workDB->anyQueryDB($query));
-        if ($count[0][0] == 0) {
-        } else {
-            $column_name = array("id");
-            $condition = "WHERE mark = '".$_POST['mark_auto_participant']."' 
-                            && `name` = '".$_POST['model_auto_participant']."'";
-            $id_auto_people_member = $workDB->selectDataTableWhere("auto", $column_name, $condition);
-        }
-
-        //Проверка уникальности гос. номера
-        $query = "SELECT count(id) FROM people WHERE state_car_number = '".$_POST['state_car_number_participant']."'";
-        $count = $workDB->analysisResult($workDB->anyQueryDB($query));
-        if ($count[0][0] == 0) {
-        } else {
-            $column_name = array("id");
-            $condition = "WHERE state_car_number = '".$_POST['state_car_number_participant']."'";
-            $state_car_number_people_culprit = $workDB->selectDataTableWhere("people", $column_name, $condition);
-        }
-        $query = "SELECT count(id) FROM people WHERE state_car_number = '".$_POST['state_car_number']."'";
-        $count = $workDB->analysisResult($workDB->anyQueryDB($query));
-        if ($count[0][0] == 0) {
-        } else {
-            $column_name = array("id");
-            $condition = "WHERE state_car_number = '".$_POST['state_car_number']."'";
-            $state_car_number_people_member = $workDB->selectDataTableWhere("people", $column_name, $condition);
-        }
-
-        //Проверка уникальности протокола
-        if (isset($id_people_member) && isset($id_people_culprit)) {
-            $query = "SELECT count(protocol.id) FROM protocol 
-				INNER JOIN polis on protocol.id_number_polis = polis.id
-                WHERE polis.number_polis = '".$_POST['number_polis']."' && protocol.id_people_culprit = ".$id_people_culprit[0][0]."
-                 && protocol.id_people_member = ".$id_people_culprit[0][0];
-            $count = $workDB->analysisResult($workDB->anyQueryDB($query));
-            if ($count[0][0] == 0) {
-
-            } else {
-                //Предупреждение о том, что такой событие уже есть
-            }
-        } else {
-
-        }
-
-
-        echo '<meta http-equiv="refresh" content="0; url=/pages/user_space.php">';
+//        echo '<meta http-equiv="refresh" content="0; url=/pages/user_space.php">';
         break;
 }
 ?>
