3270495efa4a048316f88ba3b8a17033add78a04
meltaus <meltaus@yandex.ru>
2018-06-03 10:40:22 +0700

    Описание конструктора и шаблонов классов

--- control/CheckPhoto.php
+++ control/CheckPhoto.php
@@ -1,12 +1,30 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: bunta
- * Date: 03.06.18
- * Time: 10:36
- */
 
 class CheckPhoto
 {
+    private $resultCheckDir;
+    private $resultCheckFiles=[];
+    private $rootDir;
 
+    public function __construct($absRootDir)
+    {
+        $this->rootDir = $absRootDir;
+    }
+
+    public function __destruct()
+    {
+        // TODO: Implement __destruct() method.
+    }
+
+    //На вход принимает строку из серии полича + номер полиса. Без пробела
+    public function checkExist($polis) {
+    }
+
+    public function getCheckExistDir() {
+        return $this->resultCheckDir;
+    }
+
+    public function getCheckExistFiles() {
+        return $this->resultCheckFiles;
+    }
 }
