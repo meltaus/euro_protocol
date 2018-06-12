34aa3553cd1833f18b665d6c6d440baf38334f32
meltaus <meltaus@yandex.ru>
2018-06-12 21:34:05 +0700

    Добавление кнопок и модальных окон для действий "Информация" и "Редактирование" протокола в списке всех протоколов

--- pages/user_space_mtv.php
+++ pages/user_space_mtv.php
@@ -14,9 +14,20 @@
     <link href="../css/style.css" media="all" rel="stylesheet" type="text/css"/>
 </head>
 <body>
-<?php 
+<?php
 include $_SERVER["DOCUMENT_ROOT"] . "/control/getData.php";
+include_once $_SERVER["DOCUMENT_ROOT"] . "/control/workDB.php";
 $result = createDataForMailPage();
+$query = "SELECT people.name FROM people INNER JOIN protocol ON protocol.id_people_culprit = people.id ";
+$condition = "WHERE protocol.id in (";
+for ($i = 0; $i < count($result); $i++) {
+    $condition .= $result[$i][0] . ",";
+}
+$condition = substr($condition,0,-1) . ")";
+$query .= $condition;
+$workDB = new workDB();
+$nameCulprint = $workDB->analysisResult($workDB->anyQueryDB($query));
+unset($workDB);
 ?>
 
 <div class="container">
@@ -27,7 +38,7 @@ $result = createDataForMailPage();
     </div>
     <div class="row">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <table border="1px";  style="margin:10px auto;">
+            <table style="margin:10px auto;">
                 <tr>
                     <td>
                         <div style="margin:10px auto;">
@@ -38,7 +49,6 @@ $result = createDataForMailPage();
                     </td>
                     <td>
                         <div style="margin-left: 5%; margin-right: 5%">
-                            Тут могла быть Ваша реклама
                         </div>
                     </td>
                 </tr>
@@ -46,104 +56,96 @@ $result = createDataForMailPage();
         </div>
     </div>
     <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <table border="1px"; style="margin:10px auto">
-                <tr>
-                    <td>
-                        <div style="margin-left: 5%; margin-right: 5%">
-                            Дата занесения
-                        </div>
-                    </td>
-                    <td>
-                        <div style="margin-left: 5%; margin-right: 5%">
-                            Серия/Номер виновника
-                        </div>
-                    </td>
-                    <td>
-                        <div style="margin-left: 5%; margin-right: 5%">
-                            Серия/Номер пострадавшего
-                        </div>
-                    </td>
-                    <td>
+        <table border="1px" ; style="margin:10px auto">
+            <tr>
+                <td>
+                    <div style="margin-left: 5%; margin-right: 5%">
+                        Дата занесения
+                    </div>
+                </td>
+                <td>
+                    <div style="margin-left: 5%; margin-right: 5%">
+                        ФИО виновника
+                    </div>
+                </td>
+                <td>
+                    <div style="margin-left: 5%; margin-right: 5%">
+                        Серия/Номер виновника
+                    </div>
+                </td>
+                <td>
+                    <div style="margin-left: 5%; margin-right: 5%">
+                        Серия/Номер пострадавшего
+                    </div>
+                </td>
+                <td>
 
-                    </td>					
-                </tr>
-				<tr>
-                    <td>
-                        <div style="margin-left: 5%; margin-right: 5%">
-                            12.12.1231
-                        </div>
-                    </td>
-                    <td>
-                        <div style="margin-left: 5%; margin-right: 5%">
-                            УУУ 123221234
-                        </div>
-                    </td>
-                    <td>
-                        <div style="margin-left: 5%; margin-right: 5%">
-                            ООО 3223223223
-                        </div>
-                    </td>
-                    <td>
-						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="margin-top: 10px">
-							<a href="/pages/logout.php" class="btn btn-danger pull-right">На осмотр</a>
-							<a href="#addProtocol" id="bq" class="btn btn-danger pull-right">Редактировать</a>
-						</div>
-                    </td>					
-                </tr>
-				<?php
-					for ($i = 0; $i < count($result); $i++) {
-						echo "<tr>";
-					
-						for ($j = 0; $j < count($result[$i]); $j++) {
-							
-							switch ($j) {
-								case 0:
-								break;
-								case 1:
-									echo "<td>";
-									echo "<div style=\"margin-left: 5%; margin-right: 5%\">";
-									echo $result[$i][$j];
-									echo "</div>";
-									echo "</td>";
-								break;
-								case 2:
-									echo "<td>";
-									echo "<div style=\"margin-left: 5%; margin-right: 5%\">";
-									echo $result[$i][$j];
-									echo "</div>";
-									echo "</td>";
-								break;
-								case 3:
-									echo "<td>";
-									echo "<div style=\"margin-left: 5%; margin-right: 5%\">";
-									echo $result[$i][$j];
-									echo "</div>";
-									echo "</td>";
-								break;
-								case 4:
-									if ($result[$i][$j] == null) {
-										echo "<td>";
-										echo "<div class=\"col-lg-12 col-md-12 col-sm-12 col-xs-12\" style=\"margin-top: 10px\">";
-										echo "<a href=\"#addParticipant&". $result[$i][0] ."\" id=\"btn\" class=\"btn btn-danger pull-right\">На осмотр</a>";
-										echo "</div>";
-										echo "</td>";
-									}
-									else {
-										echo "<td>";
-										echo "<div class=\"col-lg-12 col-md-12 col-sm-12 col-xs-12\" style=\"margin-top: 10px\">";
-										echo "Дата осмотра: ". $result[$i][$j];
-										echo "</div>";
-										echo "</td>";
-									}
-								break;								
-							}						
-						}						
-						echo "</tr>";
-					}
-				?>
-            </table>
-        </div>
+                </td>
+            </tr>
+            <?php
+            for ($i = 0; $i < count($result); $i++) {
+                echo "<tr>";
+
+                for ($j = 0; $j < count($result[$i]); $j++) {
+
+                    switch ($j) {
+                        case 0:
+                            break;
+                        case 1:
+                            echo "<td>";
+                            echo "<div style=\"margin-left: 5%; margin-right: 5%\">";
+                            echo $result[$i][$j];
+                            echo "</div>";
+                            echo "</td>";
+                            echo "<td>";
+                            echo "<div style=\"margin-left: 5%; margin-right: 5%\">";
+                            echo $nameCulprint[$i][0];
+                            echo "</div>";
+                            echo "</td>";
+                            break;
+                        case 2:
+                            echo "<td>";
+                            echo "<div style=\"margin-left: 5%; margin-right: 5%\">";
+                            echo $result[$i][$j];
+                            echo "</div>";
+                            echo "</td>";
+                            break;
+                        case 3:
+                            echo "<td>";
+                            echo "<div style=\"margin-left: 5%; margin-right: 5%\">";
+                            echo $result[$i][$j];
+                            echo "</div>";
+                            echo "</td>";
+                            break;
+                        case 4:
+                            if ($result[$i][$j] == null) {
+                                echo "<td>";
+                                echo "<div class=\"col-lg-12 col-md-12 col-sm-12 col-xs-12\" style=\"margin-top: 10px\">";
+                                echo "<button onclick='addParticipant(this.id)' id='" . $result[$i][0] . "'
+                                            class='btn btn-primary pull-right' 
+                                            style='margin-left: 5px'>На осмотр</button>";
+//                                    echo "<button onclick='updateProtocol(this.id)' id='" . $result[$i][0] . "
+//                                            ' class='btn btn-danger pull-right'
+//                                            style='margin-right: 5px'>Редактировать</button>";
+                                echo "</div>";
+                                echo "</td>";
+                            } else {
+                                echo "<td>";
+                                echo "<div class=\"col-lg-12 col-md-12 col-sm-12 col-xs-12\" style=\"margin-top: 10px\">";
+                                echo "Дата осмотра: " . $result[$i][$j];
+                                echo "<a href='uploadImage.php?id_protocol=" . $result[$i][0] . "' id='" . $result[$i][0] . " 
+                                            ' class='btn btn-success pull-right' 
+                                            style='margin-left: 10px'>Добавить осмотр</button>";
+                                echo "</div>";
+                                echo "</td>";
+                            }
+                            break;
+                    }
+                }
+                echo "</tr>";
+            }
+            ?>
+        </table>
     </div>
 </div>
 <!--модальные окна-->
@@ -160,23 +162,23 @@ $result = createDataForMailPage();
                         <div class="col-lg-4 col-md-1 col-sm-4 col-xs-4">
 
                             <div style="margin-top: 10%; margin-left: 5px"; class="form-group">
-								Дата осмотра: 
-								<input type="date" class="form-control" name="dateP" id="dateP" value="
-									<?php 
-									echo date('Y-m-d'); 
-									?>">
+                                Дата осмотра:
+                                <input type="datetime-local" class="form-control" name="dateP" id="dateP" value="
+									<?php
+                                echo date('Y-m-d');
+                                ?>">
                             </div>
-							<div style="margin-top: 10%; margin-left: 5px">
-								Примечание:
-								<textarea rows="15" cols="47" name="text"></textarea>
+                            <div style="margin-top: 10%; margin-left: 5px">
+                                Примечание:
+                                <textarea rows="15" cols="47" id="comment" name="comment"></textarea>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Отмена</button>
-                <button type="button" class="btn btn-primary">Далее</button>
+                <button type="submit" onclick="closeAddParticipant()" class="btn btn-default" data-dismiss="modal">Отмена</button>
+                <button type="button" onclick="addDateSee()" class="btn btn-primary">Принять</button>
             </div>
         </div>
     </div>
@@ -186,17 +188,48 @@ $result = createDataForMailPage();
 </html>
 
 <script>
-    $(function () {
-        $("#btn").click(function () {
-            $("#addParticipant").modal('show');
-        });
-    });
-	
-	$(function () {
-        $("#bq").click(function () {
-            $("#addProtocol").modal('show');
-        });
-    });
+
+    var id_protocol;
+
+    function addSee(val) {
+        document.cookie = "id_protocol=" + val;
+        id_protocol = val;
+        $("#addSee").modal('show');
+    }
+
+    function addParticipant(val) {
+        document.cookie = "id_protocol=" + val;
+        id_protocol = val;
+        $("#addParticipant").modal('show');
+    }
+
+    function updateProtocol(val) {
+        document.cookie = "id_protocol=" + val;
+        id_protocol = val;
+        window.alert("Здесь будет возможность редактировать составленный ранее протокол")
+    }
+
+    function addDateSee() {
+        var dateP = document.getElementById('dateP').value;
+        var comment = document.getElementById('comment').value;
+        if (dateP == "") {
+            window.alert("Необходимо указать дату и время");
+        } else {
+            $.ajax({
+                type: "get",
+                url: "../control/insertDataDB.php",
+                data:{'mode':'addDateSee',
+                    'id':id_protocol,
+                    'dateP':dateP,
+                    'comment':comment}
+            });
+            window.location.reload();
+        }
+    }
+
+    function closeAddParticipant(){
+        window.close();
+    }
 
     $(function () {
         $("#mark_auto").typeahead({ //на какой input:text назначить результаты списка
@@ -207,4 +240,12 @@ $result = createDataForMailPage();
             ajax: '/control/getDataWithComp.php?mode=model_auto'
         });
     })
+
+
+    $(function(){
+        $("#myTab a").click(function(e){
+            e.preventDefault();
+            $(this).tab('show');
+        });
+    });
 </script>
